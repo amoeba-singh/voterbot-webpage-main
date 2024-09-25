5246b98e944a7190c137078c3bb2e3d2b2ee0e9e
akashSingh <singhsahab2k3@gmail.com>
2024-09-26 00:24:50 +0530

    ,

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-bold">
-          Voter Sahayak
+          VoterBot
         </div>
         <div>
           <Link to="/" className="px-3 py-2 hover:text-gray-400">Home</Link>
